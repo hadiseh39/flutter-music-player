b077bea53bb2915869ee65704ea265aace8ced23
Hadiseh <hadiseh.km39@gmail.com>
2024-08-15 11:34:15 +0330

    added home page

---
+++ lib/screens/home_page.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text('Meowsics'),
+          centerTitle: true,
+        ),
+        body: ListView.builder(
+          itemCount: 10,
+          itemBuilder: (context, index){
+            return ListTile(
+              leading: ClipRRect(
+                borderRadius: BorderRadius.circular(10),
+                child: Image.asset('assets/images/57200.jpg'),
+              ),
+              title: Text('Item $index'),
+              subtitle: Text('sub'),
+              onTap: () {
+                
+              },
+            );
+          },
+        ),
+      );
+  }
+}
